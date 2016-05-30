b2f708547b1fa9207f252a3ce9b182ee168b1f8d
swapn007 <swapn007@altassian.com>
2016-05-30 00:34:59 -0700

    added suggestion

--- helloworld.c
+++ helloworld.c
@@ -2,6 +2,7 @@
 #include <conio.h>
 #include "hellokitty.h"
 
+# i am also suggesting to use
 
 #trying to add feature1
 #adding new methoding
--- helloworld.c~
+++ helloworld.c~
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <conio.h>
-
+#include "hellokitty.h"
 
 
 #trying to add feature1
